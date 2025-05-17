a1a7a02cac9f1aef197efa2f6e177b5a8873da09
Kanak <tiwaribittu1104@gmail.com>
2025-05-17 15:51:44 +0530

    mongo edit

--- server.js
+++ server.js
@@ -125,7 +125,11 @@ io.on('connection', (socket) => {
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3005'],
+  origin: [
+     'http://localhost:3000', 
+    'http://localhost:3005',
+    'https://brain-buddies-frontend-5tij.vercel.app' 
+  ],
   credentials: true,
 }));
 app.use(express.json());
@@ -144,15 +148,26 @@ app.use('/api/messages', messageRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB connected');
-    const PORT = process.env.PORT || 5000;
-    server.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
-    });
   })
   .catch(err => {
     console.error('❌ MongoDB connection error:', err);
     process.exit(1);
   });
+
+  if (process.env.NODE_ENV !== 'production') {
+  const server = http.createServer(app);
+  const io = new Server(server, {
+    cors: {
+      origin: ['http://localhost:3000', 'http://localhost:3005'],
+      methods: ['GET', 'POST'],
+      credentials: true
+    },
+  });
+   const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
+}
 
 // Error handling middleware
 app.use((err, req, res, next) => {
